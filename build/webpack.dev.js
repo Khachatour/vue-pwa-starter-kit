df1eeaae50aa19d0d950007f83d46891e37fa879
loledgamer <khachaturv@fly.co.de>
2017-05-12 20:20:49 +0400

    structure ready manifest ready cacheing ready

---
+++ build/webpack.dev.js
@@ -0,0 +1,54 @@
+'use strict'
+process.env.NODE_ENV = 'development'
+
+const webpack = require('webpack')
+const base = require('./webpack.base')
+const _ = require('./utils')
+const path = require('path')
+const FriendlyErrors = require('friendly-errors-webpack-plugin')
+const OfflinePlugin = require('offline-plugin')
+const manifestPlugin = require('pwa-manifest-webpack-plugin')
+
+base.devtool = 'eval-source-map'
+base.plugins.push(
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('development')
+  }),
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoEmitOnErrorsPlugin(),
+  new FriendlyErrors(),
+  new OfflinePlugin({
+    relativePaths: false,
+    AppCache: false,
+    ServiceWorker: {
+      events: true
+    }
+  }),
+  new manifestPlugin({
+    name: 'Sparia',
+    description: 'Sparia - Something unimaginable',
+    display: 'fullscreen',
+    icon: {
+      src: path.resolve('src/images/networking.png'),
+      sizes: [200]
+    }
+  })
+)
+
+// push loader for css files
+_.cssProcessors.forEach(processor => {
+  let loaders
+  if (processor.loader === '') {
+    loaders = ['postcss-loader']
+  } else {
+    loaders = ['postcss-loader', processor.loader]
+  }
+  base.module.loaders.push(
+    {
+      test: processor.test,
+      loaders: ['style-loader', _.cssLoader].concat(loaders)
+    }
+  )
+})
+
+module.exports = base
